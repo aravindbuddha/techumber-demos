e052d7d666d1c51559fb811bd024e5cfb849e114
aravindbuddha <buddhaaravind@gmail.com>
2014-06-29 11:39:12 +0530

    add

---
+++ blogger/main.js
@@ -0,0 +1,12 @@
+requirejs.config({
+  //By default load any module IDs from js/lib\\
+  //  urlArgs: "bust=" + (new Date()).getTime(),
+  baseUrl: 'http://demos.techumber.com/blogger/',
+  paths: {
+    jQuery: "//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"
+  }
+
+});
+require(['dPage'], function (Dpage) {
+
+});
