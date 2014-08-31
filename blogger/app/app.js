a6461549928b04472920e1a11d77dab9c302aed0
aravindbuddha <buddhaaravind@gmail.com>
2014-08-31 19:01:29 +0530

    added

--- blogger/app/app.js
+++ blogger/app/app.js
@@ -2,9 +2,9 @@ define(['require'], function (require) {
   var page_type = window.tu_page_type;
   return {
     init: function () {
-      require(['app/common', 'css!template/css/common.css', ]);
+      require(['common', 'css!template/css/common.css', ]);
       if (page_type == "index") {
-        require(['app/home', 'css!template/css/page.css']);
+        require(['home', 'css!template/css/page.css']);
       }
     }
   }
