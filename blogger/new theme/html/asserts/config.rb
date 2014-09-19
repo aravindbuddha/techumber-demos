6e16bebe61875293aeb19542fdcb1e4b09dd58ac
aravind buddha <buddhaaravind@gmail.com>
2014-09-19 19:59:46 +0530

    added

--- blogger/new theme/html/asserts/config.rb
+++ blogger/new theme/html/asserts/config.rb
@@ -1,5 +1,6 @@
-require 'compass/import-once/activate'
-require 'bootstrap-sass'
+require 'compass'
+
+# require 'bootstrap-sass'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
