405f42991d50747bf05eab505af1b64cb1c3286d
aravind buddha <buddhaaravind@gmail.com>
2014-10-18 20:10:20 +0530

    added

--- cdn/app/init.js
+++ cdn/app/init.js
@@ -1,7 +1,7 @@
 var App = (function(){
   var
-    base_url="//demos.techumber.com/cdn/",
-    site_array=[
+    base_url="//demos.techumber.com/cdn/";
+  var site_array=[
       base_url+"asserts/css/style.css",
       "//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js",
       base_url+"js/lib/easing.js",
@@ -30,38 +30,66 @@ var App = (function(){
 
   return {
     init:function(){
-      head.ready(document,function(){
-            //load hole site
-            head.load(site_array,function(){
-              console.log("whole site loadded....");
-              //test page type
-              if(TU_PAGE == "index"){
-               $("body").addClass('index').removeClass('static item');
-                head.load(index_array,function(){
-                  console.log("index array loadded....");
-                });
-              }
-              if(TU_PAGE == "item"){
-                head.load(item_array,function(){
-                  $("body").addClass('item').removeClass('static index');
-                  console.log("item array loadded....");
-                });
-              }
-
-              if(TU_PAGE == "static_page"){
-                head.load(static_array,function(){
-                    $("body").addClass('static').removeClass('item index');
-                  console.log("item array loadded....");
-                });
-              }
-
+       head.ready(document,function(){
+         console.log("hi");
+          if(TU_PAGE == "index"){
+            $("body").addClass('index').removeClass('static item');
+              head.load(index_array,function(){
+                console.log("index array loadded....");
+              });
+          }
+          if(TU_PAGE == "item"){
+            head.load(item_array,function(){
+              $("body").addClass('item').removeClass('static index');
+              console.log("item array loadded....");
             });
-      
-      });
+          }
+          if(TU_PAGE == "static_page"){
+            head.load(static_array,function(){
+              $("body").addClass('static').removeClass('item index');
+              console.log("item array loadded....");
+            });
+          }
+
+       });
     }
+   
   }
 
 }());
+
+ // init:function(){
+    //   head.ready(document,function(){
+    //         //load hole site
+    //         head.load(site_array,function(){
+    //           console.log("whole site loadded....");
+    //           //test page type
+    //           if(TU_PAGE == "index"){
+    //            $("body").addClass('index').removeClass('static item');
+    //             head.load(index_array,function(){
+    //               console.log("index array loadded....");
+    //             });
+    //           }
+    //           if(TU_PAGE == "item"){
+    //             head.load(item_array,function(){
+    //               $("body").addClass('item').removeClass('static index');
+    //               console.log("item array loadded....");
+    //             });
+    //           }
+
+    //           if(TU_PAGE == "static_page"){
+    //             head.load(static_array,function(){
+    //                 $("body").addClass('static').removeClass('item index');
+    //               console.log("item array loadded....");
+    //             });
+    //           }
+
+    //         });
+      
+    //   });
+    // }
+
+
 
 App.init();
 
