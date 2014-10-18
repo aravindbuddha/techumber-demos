e942b427d2991aa00fdb78f004a614d707c11e53
aravind buddha <buddhaaravind@gmail.com>
2014-10-18 15:27:45 +0530

    added

--- cdn/js/site.js
+++ cdn/js/site.js
@@ -3,7 +3,7 @@ var Site = (function() {
     return {
         init: function() {
             this.moto_init();
-            this.mob_nav_init();
+            // this.mob_nav_init();
             this.jpre_loader_init();
             this.accordx('#sidebar-wtengah');
             this.hoverxa();
