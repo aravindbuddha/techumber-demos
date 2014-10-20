194b1897d268b886ae0717b72ac002f9375ad96c
aravind buddha <buddhaaravind@gmail.com>
2014-10-20 15:24:53 +0530

    added

--- cdn/app/dpage.js
+++ cdn/app/dpage.js
@@ -1,11 +1,11 @@
-define(['jquery','app/app'],function($,App){
+var Dpage=(function(){
   var
     $my_domain = window.location.host,
     $main = $('#main-wrapper');
-    console.log(App);
   return {
     init: function () {
       this.events();
+      ///
     },
     events: function () {
       var self = this;
@@ -38,8 +38,11 @@ define(['jquery','app/app'],function($,App){
       return false;
     },
     triggers: function () {
-      App.init();
+      App.load_all();
       this.events();
+    },
+    set_class:function(){
+        
     },
     load_comments: function () {
       BLOG_CMT_createIframe('https://www.blogger.com/rpc_relay.html', '05812328105391254280');
@@ -47,60 +50,7 @@ define(['jquery','app/app'],function($,App){
       //
     }
   }
-});
-// var Dpage=(function(){
-//   var
-//     $my_domain = window.location.host,
-//     $main = $('#main-wrapper');
-//   return {
-//     init: function () {
-//       this.events();
-//       ///
-//     },
-//     events: function () {
-//       var self = this;
-//       $('a').on('click', function (e) {
-//         var url = $(this).attr('href');
-//         console.log(self.is_my_domain(url));
-//         if(self.is_my_domain(url)){
-//           e.preventDefault();
-//           history.pushState(null, null, url);
-//           self.load_content(url);
-//         }
-//       });
-//     },
-//     load_content: function (url) {
-//       var self = this;
-//       if (this.is_my_domain(url)) {
-//         console.log("loading " + url + ">>>>>>>>>>");
-//         $("#main-wrapper").css({'opacity': 0.5});
-//         $main.load(url + " #main", function () {
-//           window.TU_PAGE=$('.tu_type').data('value');
-//           $("#main-wrapper").css({'opacity': 1});
-//           self.triggers();
-//         });
-//       }
-//     },
-//     is_my_domain: function (url) {
-//       if (url.indexOf($my_domain) > -1) {
-//         return true;
-//       }
-//       return false;
-//     },
-//     triggers: function () {
-//       App.load_all();
-//       this.events();
-//     },
-//     set_class:function(){
-        
-//     },
-//     load_comments: function () {
-//       BLOG_CMT_createIframe('https://www.blogger.com/rpc_relay.html', '05812328105391254280');
 
-//       //
-//     }
-//   }
+}());
 
-// }());
-
-// Dpage.init();
+Dpage.init();
