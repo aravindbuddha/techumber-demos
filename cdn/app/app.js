93fc868ea641f94973f933fb813f05c5c0b9fc63
aravind buddha <buddhaaravind@gmail.com>
2014-10-20 14:48:26 +0530

    added

--- cdn/app/app.js
+++ cdn/app/app.js
@@ -1,33 +1,26 @@
 define(["jquery","easing","motio","preload","hover","site","dpage"],function($,easing,motio,preload,hover,site,dpage){
 	return {
 		init:function(){
-			 if(TU_PAGE == "index"){
-		        $("body").addClass('index').removeClass('static item');
-		        require(["isotop","index"],function(){
-		        	  console.log(" index loadded....");
-		        });
-		      }
-		      if(TU_PAGE == "item"){
-		        require(['pretty','related','item'],function(){
-		          $("body").addClass('item').removeClass('static index');
-		          console.log("item array loadded....");
-		        });
-		      }
-		      // if(TU_PAGE == "static_page"){
-		      //   head.load(static_array,function(){
-		      //     $("body").addClass('static').removeClass('item index');
-		      //     console.log("item array loadded....");
-		      //   });
-		      // }
+			if(TU_PAGE == "index"){
+				this.load_index();
+			}else if(TU_PAGE == "item"){
+				this.load_item();
+			}
 		},
 		load_site:function(){
 
 		},
 		load_item:function(){
-
+		   	require(['pretty','related','item'],function(){
+	          $("body").addClass('item').removeClass('static index');
+	          console.log("item array loadded....");
+	        });
 		},
 		load_index:function(){
-
+			 $("body").addClass('index').removeClass('static item');
+		        require(["isotop","index"],function(){
+		        	  console.log(" index loadded....");
+		        });
 		}
 	}
 });
