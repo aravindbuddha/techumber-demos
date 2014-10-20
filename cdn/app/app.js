934c2cbc5afa12a367c793e658d1d48f6bc52c2a
aravind buddha <buddhaaravind@gmail.com>
2014-10-20 14:38:49 +0530

    added

--- cdn/app/app.js
+++ cdn/app/app.js
@@ -1,4 +1,4 @@
-define(["jquery","easing"],function($,easing){
+define(["jquery","easing","motio","preload","hover","site","dpage"],function($,easing,motio,preload,hover,site,dpage){
 	return {
 		init:function(){
 			console.log($);
